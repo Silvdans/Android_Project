d27f02e03f5a1b0f73ccedcb94d2ee6f3d3c435f
= <loic.delpierre16@gmail.com>
2021-12-20 19:59:22 +0100

    fix header

--- app/src/main/java/fr/epsi/android_project/CategoryActivity.kt
+++ app/src/main/java/fr/epsi/android_project/CategoryActivity.kt
@@ -3,9 +3,10 @@ package fr.epsi.android_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CategoryActivity : AppCompatActivity() {
+class CategoryActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+        showBack();
     }
 }
